b8947142a3fc5cee6d1d506744f5904c039ac79c
MAMAlghannam <MAMAlghannam@outlook.com>
2020-02-05 19:05:25 +0300

    Post, Question buttons in Add Screen

--- Screens/Add.js
+++ Screens/Add.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, Button, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Dimensions } from 'react-native';
 
 export default class Add extends React.Component {
   render(){
   return (
-    <View>
-        <Text>Add</Text>
-        <Button title="Post" onPress={()=>this.props.navigation.navigate('AddPost')}/>
-        <Button title="Question" onPress={()=>this.props.navigation.navigate('AddQuestion')}/>
+  <View style={styles.container}>
+    <View style={[styles.buttonContainer, {backgroundColor: 'green', }]}>
+    <TouchableOpacity 
+      style={styles.button} 
+      onPress={()=>this.props.navigation.navigate('AddPost')}
+    >
+      <Text style={styles.text}>Post</Text>
+    </TouchableOpacity>
     </View>
+
+    <View style={[styles.buttonContainer, {backgroundColor: 'blue'}]}>
+    <TouchableOpacity 
+      style={styles.button} 
+      onPress={()=>this.props.navigation.navigate('AddQuestion')}
+    >
+      <Text style={styles.text}>Question</Text>
+    </TouchableOpacity>
+    </View>
+  </View>
   );
   }
 }
@@ -16,8 +30,25 @@ export default class Add extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer:{
+    width: Dimensions.get('window').width*0.7,
+    margin: Dimensions.get('window').width*0.15,
+    padding: 3,
+    borderRadius: 4
+  },  
+  button:{
+    borderRadius: 4,
+    padding: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 3,
+    borderColor: 'white'
+  },
+  text:{
+    fontSize: 22,
+    color: 'white'
+  }
 });
